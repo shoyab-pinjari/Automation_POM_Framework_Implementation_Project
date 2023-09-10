e7e1f7a061a4b31c6069fbfecebc1da1ca1e0941
shoyab-pinjari <shoyab.pinjari@yahoo.com>
2023-09-10 06:26:20 +0530

    Added

---
+++ General_practice/trial3.py
@@ -0,0 +1,16 @@
+# s = input("Enter string: ")
+# subs = input("Subs: ")
+# pos=-1
+# flag = False
+# n=len(s)
+# while True:
+#     pos=s.find(subs, pos+1, n)
+#     if pos ==-1:
+#         break
+#     else:
+#         print("Found at position: ",pos)
+#     flag = True
+# if flag ==False:
+#     print("Not found")
+#
+
